1861f429b7f33998a5ca34194e1711032b0603d0
Christian <christian.m.cabato@student.uts.edu.au>
2017-10-19 12:35:15 +1100

    Updated Web App Interface with login and a 'better' looking UI
    Username: python
    Password: python
    localhost:5000

--- credit/run.py
+++ credit/run.py
@@ -1,13 +1,42 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, session, abort
 from flask_sqlalchemy import SQLAlchemy
 from models import db, app, Credit
+import os
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
+engine = create_engine('sqlite:///tutorial.db', echo=True)
 
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:xtian211@localhost:5432/creditdb' 
 
 @app.route("/")
 def index():
-    return render_template('index.html', credit = Credit.query.all())
+	if not session.get('logged_in'):
+		return render_template('login.html')
+	else:
+		return render_template('index.html', credit = Credit.query.all())
+
+@app.route("/login", methods=['POST'])
+def do_admin_login():
+	
+	POST_USERNAME = str(request.form['username'])
+	POST_PASSWORD = str(request.form['password'])
+
+	Session = sessionmaker(bind=engine)
+	s = Session()
+	query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+	result = query.first()
+	if result:
+		session['logged_in'] = True
+	else:
+		flash('wrong password!')
+	return index()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return index()
+
 
 @app.route("/search", methods=['GET', 'POST'])
 def search():
@@ -17,4 +46,5 @@ def search():
 	return render_template('results.html', credits=credits)
 
 if __name__ == '__main__':
-    app.run()
+	app.secret_key = os.urandom(12)
+	app.run()
